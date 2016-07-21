2fddabf08411c1a2239b1dd751f564ffbbeb3664
Hutsh <hxbbun@gmail.com>
2016-07-21 19:01:08 +0800

    mod
    点击按钮获取剪贴板并替换剪贴板

--- cbformat.py
+++ cbformat.py
@@ -1,8 +1,13 @@
 #coding=utf-8
+import sys
+from PyQt5 import QtCore, QtGui, uic, QtWidgets
 import win32clipboard
 import win32con
 import re
-from tkinter import *
+
+qtCreatorFile = "ClipboardFormater.ui"
+
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
 def get_clipboard():
 	win32clipboard.OpenClipboard()
@@ -16,28 +21,42 @@ def set_clipboard(text):
 	win32clipboard.SetClipboardText(text)
 	win32clipboard.CloseClipboard()
 
-# -----------可用----------------
-# cb=get_clipboard()
-# print(cb)
-# print("\n\n")
-
-# string =cb
-# p = r'(\r\n)([ABCD])'
-# pattern = re.compile(p)
-
-# res = pattern.sub(r'\1\2. ',string)
-# print(res)
-# set_clipboard(res)
-# -----------可用----------------
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+	def __init__(self):
+		QtWidgets.QMainWindow.__init__(self)
+		Ui_MainWindow.__init__(self)
+		self.setupUi(self)
+		self.apply1_button.clicked.connect(self.Button1)
+		self.apply2_button.clicked.connect(self.Button2)
 
 
-root=Tk()
-root.title("Clipboard Formater")
-root.geometry('400x300')
+	def focusInEvent(self, event):
+		self.cbtext.setText('Got focus')
+
+	def focusOutEvent(self, event):
+		self.cbtext.setText('Lost focus')
+
+	def Button1(self):
+		string=get_clipboard()
+		self.cbtext.setText(string)
+		pattern=self.regex1.toPlainText()
+		rep=self.replace1.toPlainText()
+		pattern=re.compile(pattern)
+		res = pattern.sub(rep,string)
+		self.replaced_text.setText(res)
+		set_clipboard(res)
+	def Button2(self):
+		string=get_clipboard()
+		self.cbtext.setText(string)
+		pattern=self.regex2.toPlainText()
+		rep=self.replace2.toPlainText()
+		pattern=re.compile(pattern)
+		res = pattern.sub(rep,string)
+		self.replaced_text.setText(res)
 
 
-
-
-
-
-root.mainloop()
+if __name__ == "__main__":
+	app = QtWidgets.QApplication(sys.argv)
+	window = MyApp()
+	window.show()
+	sys.exit(app.exec_())
