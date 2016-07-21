2752caa17eb26bec7396c40cb469c86b44c6fe3c
Hutsh <hxbbun@gmail.com>
2016-07-21 18:06:39 +0800

    add ui

--- cbformat.py
+++ cbformat.py
@@ -16,26 +16,28 @@ def set_clipboard(text):
 	win32clipboard.SetClipboardText(text)
 	win32clipboard.CloseClipboard()
 
+# -----------可用----------------
+# cb=get_clipboard()
+# print(cb)
+# print("\n\n")
 
-cb=get_clipboard()
-print(cb)
-print("\n\n")
+# string =cb
+# p = r'(\r\n)([ABCD])'
+# pattern = re.compile(p)
 
-string =cb
-p = r'(\r\n)([ABCD])'
-pattern = re.compile(p)
-
-res = pattern.sub(r'\1\2. ',string)
-print(res)
-set_clipboard(res)
-
-# root=Tk()
-# root.title("Clipboard Formater")
-# root.geometry('400x300')
+# res = pattern.sub(r'\1\2. ',string)
+# print(res)
+# set_clipboard(res)
+# -----------可用----------------
 
 
+root=Tk()
+root.title("Clipboard Formater")
+root.geometry('400x300')
 
 
 
 
-# root.mainloop()
+
+
+root.mainloop()
